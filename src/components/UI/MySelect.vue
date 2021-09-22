4257cc3ed2ff245c5cc5a36f321e3dd91a58cd52
Light <nikita.erychev@mail.ru>
2021-09-22 16:55:43 +0300

    MySelect

---
+++ src/components/UI/MySelect.vue
@@ -0,0 +1,17 @@
+<template>
+    <select name="" id="">
+        <option disabled value="">Choose from list</option>
+        //1:25
+    </select>
+</template>
+
+<script>
+export default {
+    name: 'my-select',
+    
+}
+</script>
+
+<style scoped>
+
+</style>
